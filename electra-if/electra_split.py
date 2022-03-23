3d7c3bb947b35f4a56f61ff51dd163c9d7dc1297
Aureliano Porporato <aureliano.porporato@gmail.com>
2022-03-23 21:42:54 +0100

    Labels update

--- electra-if/electra_split.py
+++ electra-if/electra_split.py
@@ -6,7 +6,7 @@ data = pd.read_csv('actions_stripped_set_sorted_aug-ann.csv', sep=';', header=0)
 base = pd.read_csv('common_commands.csv', sep=';')
 base_action = set([s.replace('_', ' ').lower() for c in base['VERB'].tolist() if not pd.isna(c) for s in c.split(',')])
 base_action.union([s.lower() for c in base['ABBR'].tolist() if not pd.isna(c) for s in c.split(',')])
-base_wn = set([s.lower() for c in base['WN_OFFSET'].tolist() if not pd.isna(c) for s in c.split(',')])
+base_wn = set([s.lower() for c in base['WORDNET'].tolist() if not pd.isna(c) for s in c.split(',')])
 base_vn = set([s.lower() for c in base['VERBNET'].tolist() if not pd.isna(c) for s in c.split(',')])
 base_fn = set([s.lower() for c in base['FRAMENET'].tolist() if not pd.isna(c) for s in c.split(',')])
 
@@ -16,43 +16,43 @@ fn_dict = {}
 npc_dict = {}
 for index, row in data.iterrows():
     # Wordnet
-    wn_offsets = row['WN_OFFSET']
-    if pd.isna(row['WN_OFFSET']):
-        wn_offsets = 'null'
-    wn_offsets = wn_offsets.split(',')
-    for wn_offset in wn_offsets:
-        if wn_offset not in wn_dict.keys():
-            wn_dict[wn_offset] = []
-        wn_dict.get(wn_offset).append(row['ACTION'])
+    wn_labels = row['WORDNET']
+    if pd.isna(row['WORDNET']):
+        wn_labels = 'unknown'
+    wn_labels = wn_labels.split(',')
+    for wn_label in wn_labels:
+        if wn_label not in wn_dict.keys():
+            wn_dict[wn_label] = []
+        wn_dict.get(wn_label).append(row['ACTION'])
     # Verbnet
-    vn_offsets = row['VERBNET']
+    vn_labels = row['VERBNET']
     if pd.isna(row['VERBNET']):
-        vn_offsets = 'null'
-    vn_offsets = vn_offsets.split(',')
-    for vn_offset in vn_offsets:
-        if vn_offset not in vn_dict.keys():
-            vn_dict[vn_offset] = []
-        vn_dict.get(vn_offset).append(row['ACTION'])
+        vn_labels = 'unknown'
+    vn_labels = vn_labels.split(',')
+    for vn_label in vn_labels:
+        if vn_label not in vn_dict.keys():
+            vn_dict[vn_label] = []
+        vn_dict.get(vn_label).append(row['ACTION'])
     # Framenet
-    fn_offsets = row['FRAMENET']
+    fn_labels = row['FRAMENET']
     if pd.isna(row['FRAMENET']):
-        fn_offsets = 'null'
-    fn_offsets = fn_offsets.split(',')
-    for fn_offset in fn_offsets:
-        if fn_offset not in fn_dict.keys():
-            fn_dict[fn_offset] = []
-        fn_dict.get(fn_offset).append(row['ACTION'])
+        fn_labels = 'unknown'
+    fn_labels = fn_labels.split(',')
+    for fn_label in fn_labels:
+        if fn_label not in fn_dict.keys():
+            fn_dict[fn_label] = []
+        fn_dict.get(fn_label).append(row['ACTION'])
     # Third person command
-    npc_offsets = row['NPC']
+    npc_labels = row['NPC']
     if pd.isna(row['NPC']):
-        npc_offsets = 'null'
+        npc_labels = 'unknown'
     else:
-        npc_offsets = str(int(npc_offsets))
-    npc_offsets = npc_offsets.split(',')
-    for npc_offset in npc_offsets:
-        if npc_offset not in npc_dict.keys():
-            npc_dict[npc_offset] = []
-        npc_dict.get(npc_offset).append(row['ACTION'])
+        npc_labels = str(int(npc_labels))
+    npc_labels = npc_labels.split(',')
+    for npc_label in npc_labels:
+        if npc_label not in npc_dict.keys():
+            npc_dict[npc_label] = []
+        npc_dict.get(npc_label).append(row['ACTION'])
 
 with open("wn\\wn_full.json", "w") as file:
     file.write(json.dumps(wn_dict, indent=2))
